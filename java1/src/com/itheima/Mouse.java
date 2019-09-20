03de0072f3ce36c597fc8ca151ff3fa76ae55f8f
zhz <1414828626@qq.com>
2019-09-20 10:50:39 +0800

    添加了hello word

---
+++ java1/src/com/itheima/Mouse.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+/**
+ * @author zhz
+ * @describe 测试类
+ * @date 2019/9/20 10:48
+ */
+public class Mouse {
+
+    public void sayHello() {
+        System.out.println("hello word");
+    }
+}
